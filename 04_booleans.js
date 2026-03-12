b5ccc19f2557c13dfae71e037a244a0e0ee2c21c
abhishek rawat <abhishekrawat704235@gmail.com>
2026-03-12 11:25:00 +0530

    bhai aaj maine yah kiya

---
+++ 04_booleans.js
@@ -0,0 +1,16 @@
+console.log(true);
+console.log(false);
+
+//logical operators
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+console.log(0.2 >= 0 && 0.2 < 3/1); // true
+
+//logical OR operator
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true 
+console.log(false || false); // false
+console.log(0.2 >= 0 || 0.2 < 3/1); // true 
---
+++ 05_if_statement.js
@@ -0,0 +1,19 @@
+if(false){
+    console.log("this is true");
+} else {
+    console.log("this is false");
+}
+
+
+
+//practice queation 1 . write a code to check if someone is old enough to drive. The legal driving age is 18.
+
+if(age >= 18){
+    console.log("you are can drive");
+}
+ else if(age >= 18){
+    console.log("allow you to drive");
+}
+else{
+    console.log("you are not old enough to drive"); 
+}
